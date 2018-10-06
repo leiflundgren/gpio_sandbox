924935fe0a86d4d526c7fa099516bef7b2d96f4b
Leif Lundgren <l@l>
2018-10-06 23:27:27 +0200

    Managed to have LED steady on

--- src/gpio1.py
+++ src/gpio1.py
@@ -1,5 +1,15 @@
 import RPi.GPIO as GPIO
+import time
+
+LED = 32			#pin no. as per BOARD, GPIO18 as per BCM
+
 
 #set up GPIO using BCM numbering
-GPIO.setmode(GPIO.BCM)
-print GPIO.RPI_REVISION
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(LED, GPIO.OUT)	#set GPIO as output
+GPIO.output(LED, GPIO.HIGH)
+print('pin ' + str(LED) + ' is high')
+time.sleep(40)
+
+GPIO.cleanup()
